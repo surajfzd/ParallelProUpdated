8ccf6df16a034ec6d1f87307fdc8185e83ce13c4
surajfzd <46921392+surajfzd@users.noreply.github.com>
2019-03-11 16:03:21 +0530

    Add files via upload

---
+++ ProductCartSpringBootApplication(Jpa Implementation)/src/main/java/com/capgemini/exception/DuplicateProductIdException.java
@@ -0,0 +1,14 @@
+package com.capgemini.exception;
+
+public class DuplicateProductIdException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+public DuplicateProductIdException(String msg)
+{
+	super(msg);
+}
+}
